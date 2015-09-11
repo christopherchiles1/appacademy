87872e307c98fa9c97426bed5a7791d52984b237
christopherchiles1 <christopher.chiles1@gmail.com>
2015-09-11 13:51:14 -0700

    Finished ActiveRecordLite SQLObject

--- sql-curriculum/w3d5/lib/01_sql_object.rb
+++ sql-curriculum/w3d5/lib/01_sql_object.rb
@@ -3,11 +3,11 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    columns = DBConnection.execute2(<<-SQL).first.map(&:to_sym)
+    @columns ||= DBConnection.execute2(<<-SQL).first.map(&:to_sym)
       SELECT
         *
       FROM
-        '#{table_name}';
+        #{table_name};
     SQL
   end
 
@@ -32,7 +32,7 @@ class SQLObject
       SELECT
         *
       FROM
-        "#{self.table_name}"
+        #{self.table_name}
     SQL
     self.parse_all(results)
   end
@@ -42,7 +42,16 @@ class SQLObject
   end
 
   def self.find(id)
-    # ...
+    results = DBConnection.execute(<<-SQL, id)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+      WHERE
+        id = ?
+      LIMIT 1;
+    SQL
+    self.parse_all(results).first
   end
 
   def initialize(params = {})
@@ -60,18 +69,36 @@ class SQLObject
   end
 
   def attribute_values
-    attributes.values
+    self.class.columns.map { |col| send(col) }
   end
 
   def insert
-    # ...
+    col_names = self.class.columns.map(&:to_s).join(", ")
+    sql_marks = (['?'] * self.class.columns.count).join(", ")
+    DBConnection.execute(<<-SQL, *attribute_values)
+      INSERT INTO
+        #{self.class.table_name} (#{col_names})
+      VALUES
+        (#{sql_marks});
+    SQL
+
+    self.id = DBConnection.last_insert_row_id
   end
 
   def update
-    # ...
+    set_line = self.class.columns.map { |attr| attr.to_s + ' = ?'}.join(", ")
+
+    DBConnection.execute(<<-SQL, *attribute_values, id)
+      UPDATE
+        #{self.class.table_name}
+      SET
+        #{set_line}
+      WHERE
+        id = ?;
+    SQL
   end
 
   def save
-    # ...
+    id.nil? ? insert : update
   end
 end
