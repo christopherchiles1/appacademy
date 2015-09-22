8ce08568854c49b843f0f20909c9741a88eb198e
christopherchiles1 <christopher.chiles1@gmail.com>
2015-09-22 15:19:09 -0700

    Rails lite phase 04 completed

--- rails-curriculum/w5d2/lib/phase4/session.rb
+++ rails-curriculum/w5d2/lib/phase4/session.rb
@@ -6,17 +6,24 @@ module Phase4
     # find the cookie for this app
     # deserialize the cookie into a hash
     def initialize(req)
+      @req = req
+      cookie = req.cookies.find { |cookie| '_rails_lite_app' == cookie.name }
+      @cookie = cookie ? JSON.parse(cookie.value) : {}
     end
 
     def [](key)
+      @cookie[key]
     end
 
     def []=(key, val)
+      @cookie[key] = val
     end
 
     # serialize the hash into json and save in a cookie
     # add to the responses cookies
     def store_session(res)
+      json_cookie = @cookie.to_json
+      res.cookies << WEBrick::Cookie.new('_rails_lite_app', json_cookie)
     end
   end
 end
