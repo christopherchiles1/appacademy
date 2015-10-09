deeacd73b623afe534c01f5c1a8093ad6af46f21
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-09 11:00:12 -0700

    Created bench model and api backend

--- react-flux-curriculum/w7d5/db/seeds.rb
+++ react-flux-curriculum/w7d5/db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+1000.times do
+  Bench.create!(
+    { description: Faker::Lorem.sentence,
+      lat: Faker::Address.latitude,
+      lon: Faker::Address.longitude
+    }
+  )
+end
