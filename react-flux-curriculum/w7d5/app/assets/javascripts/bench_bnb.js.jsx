65f89d2ad0fdda6f169648796f4a2e0ee3ab94a2
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-09 13:31:27 -0700

    Rendering the fetched json for benches

--- react-flux-curriculum/w7d5/app/assets/javascripts/bench_bnb.js.jsx
+++ react-flux-curriculum/w7d5/app/assets/javascripts/bench_bnb.js.jsx
@@ -1,3 +1,26 @@
 $(function () {
+
+  BenchBnBApp.Index = React.createClass({
+    getInitialState: function () {
+      return { benches: BenchBnBApp.BenchStore.all() };
+    },
+    componentDidMount: function () {
+      BenchBnBApp.BenchStore.addChangeListener(this._updateBenches);
+      BenchBnBApp.ApiUtil.fetchBenches();
+    },
+    _updateBenches: function () {
+      this.setState({ benches: BenchBnBApp.BenchStore.all() });
+    },
+    render: function () {
+      return (
+        <div>{this.state.benches}</div>
+      );
+    }
+  });
+
   // Render the top-level React component
+  React.render(
+    <BenchBnBApp.Index />,
+    document.getElementById("content")
+  );
 });
--- react-flux-curriculum/w7d5/app/assets/javascripts/stores/bench_store.js
+++ react-flux-curriculum/w7d5/app/assets/javascripts/stores/bench_store.js
@@ -10,16 +10,24 @@
     _benches = benches;
   };
 
+  var CHANGE_EVENT = "CHANGE_EVENT";
+
   BenchBnBApp.BenchStore = $.extend({}, EventEmitter.prototype, {
     // NOTE: The BenchStore public API should only have getters
     all: function () {
       return _benches.slice();
     },
+    addChangeListener: function (callback) {
+      this.addListener(CHANGE_EVENT, callback);
+    },
+    removeChangeListener: function (callback) {
+      this.removeListener(CHANGE_EVENT, callback);
+    },
     dispatcherId: BenchBnBApp.AppDispatcher.register(function (payload) {
       if (payload.actionType === BenchBnBApp.BenchConstants.BENCHES_RECEIVED) {
         _resetBenches(payload.benches);
+        BenchBnBApp.BenchStore.emit(CHANGE_EVENT);
       }
     })
   });
-
 })(this);
