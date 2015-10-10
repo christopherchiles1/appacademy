0646d88e9523e76958b6fe1ac145767ea50eda8c
christopherchiles1 <christopher.chiles1@gmail.com>
2015-10-09 17:32:10 -0700

    Working on bench marker filtering! --DOES NOT WORK NOW--

--- react-flux-curriculum/w7d5/app/assets/javascripts/components/map.js.jsx
+++ react-flux-curriculum/w7d5/app/assets/javascripts/components/map.js.jsx
@@ -6,6 +6,17 @@
   var BenchBnBApp = root.BenchBnBApp;
 
   BenchBnBApp.Map = React.createClass({
+    getInitialState: function () {
+      this._createMap();
+      this.markers = [];
+      this.benchIds = [];
+    },
+
+    componentDidMount: function(){
+      this._setMapIdleHandler();
+      BenchBnBApp.BenchStore.addChangeListener(this._updateBenches);
+    },
+
     _createMap: function () {
       var map = React.findDOMNode(this.refs.map);
       var mapOptions = {
@@ -13,6 +24,10 @@
         zoom: 13
       };
       this.map = new root.google.maps.Map(map, mapOptions);
+    },
+
+    _setMapIdleHandler: function () {
+      this.map.addListener('idle', this._onMapIdle);
     },
 
     _onMapIdle: function () {
@@ -26,29 +41,53 @@
       BenchBnBApp.ApiUtil.fetchBenches({ bounds: bounds });
     },
 
-    _setMapIdleHandler: function () {
-      this.map.addListener('idle', this._onMapIdle);
-    },
-
     _updateBenches: function () {
       var benches = BenchBnBApp.BenchStore.all();
-      console.log(benches);
-      // Add markers to Map may need to be an ivar
-      var markers = benches.map(function (bench) {
-        return (
-          new root.google.maps.Marker({
-            position: {lat: bench.lat, lng: bench.lon},
-            map: this.map,
-            title: 'Hello World!' // TODO: change this
-          })
-        );
-      }.bind(this));
+
+      var markers = [];
+      _addedBenches.forEach(function (bench) {
+        _addMarker(bench);
+      });
+      _removedBenches.forEach(function (bench) {
+        _removeMarker(bench);
+      });
+
+      this.oldBenches = benches;
     },
 
-    componentDidMount: function(){
-      this._createMap();
-      this._setMapIdleHandler();
-      BenchBnBApp.BenchStore.addChangeListener(this._updateBenches);
+    _addedBenches: function (oldList, newList) {
+      var added = newList.filter(function (bench) {
+        var included = false;
+        oldList.forEach(function (oldBench) {
+          if (bench.id === oldBench.id) {
+            included = true;
+          }
+        });
+        return !included;
+      });
+      console.log(added);
+    },
+
+    _removedBenches: function (oldList, newList) {
+      // TODO: Get marker filtering to work!!!
+    },
+
+    _addMarker: function (bench) {
+      var marker = new root.google.maps.Marker({
+        position: {lat: bench.lat, lng: bench.lon},
+        map: this.map,
+        title: 'Hello World!' // TODO: change this
+      });
+      this.markers.push([board.id, marker]);
+    },
+
+    _removeMarker: function (board) {
+      var marker = this.markers.find(function (marker) {
+        return marker[0] === board.id;
+      });
+      var idx = this.markers.indexOf(marker);
+      this.markers.slice(idx, 1);
+      marker.setMap(null);
     },
 
     render: function () {
